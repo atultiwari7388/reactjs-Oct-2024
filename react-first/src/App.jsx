1b1b0cd91bd26cab14ebc591c370b278d9765866
Atul Tiwari <tiwariatul9526@gmail.com>
2024-10-24 18:22:08 +0530

    jsx done

--- react-first/src/App.jsx
+++ react-first/src/App.jsx
@@ -1,5 +1,26 @@
-const App = () => {
-  return <section>Hello This is Section Part</section>;
-};
+export default function App() {
+  let user = "kalua";
+  const age = 20;
 
-export default App;
+  const changeUserBtn = () => {
+    user = "Aryan";
+    console.log(`Our New user is ${user}`);
+  };
+
+  return (
+    <div>
+      <h1>
+        Hello my name is {user} and my age is {age}
+      </h1>
+      <button style={buttonStyle} onClick={changeUserBtn}>
+        Change User
+      </button>
+    </div>
+  );
+}
+
+const buttonStyle = {
+  color: "blue",
+  backgroundColor: "lightseagreen",
+  padding: "5px 10px",
+};
