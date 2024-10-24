8b8362b4c3321e734bcd965dbfaf326ce2f2250a
Atul Tiwari <tiwariatul9526@gmail.com>
2024-10-24 20:10:30 +0530

    two way binding

--- react-first/src/components/Form.jsx
+++ react-first/src/components/Form.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
+
 export const FormComponent = () => {
+  const [userName, setUserName] = useState("");
+
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log("Submit");
+    console.log(userName);
+    setUserName("");
   };
 
   return (
@@ -15,6 +20,10 @@ export const FormComponent = () => {
         className="px-4 py-3 rounded border m-2"
         type="text"
         placeholder="Enter your name"
+        value={userName}
+        onChange={(e) => {
+          setUserName(e.target.value);
+        }}
       />
       <button className="px-4 py-3 font-semibold bg-cyan-400 text-white rounded m-2">
         Submit
