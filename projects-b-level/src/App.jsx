6bbc9558b91eb9efa03bd70041d79f2dce596851
Atul Tiwari <tiwariatul9526@gmail.com>
2024-10-29 14:12:02 +0530

    toggle-background-proj

--- projects-b-level/src/App.jsx
+++ projects-b-level/src/App.jsx
@@ -1,13 +1,15 @@
 // import { Counter } from "./components/Counter";
 // import Meal from "./components/Meal";
-import { Calculator } from "./components/Calculator";
+// import { Calculator } from "./components/Calculator";
+import ToggleBackground from "./components/ToggleBackground";
 
 const App = () => {
   return (
     <div>
       {/* <Counter /> */}
       {/* <Meal /> */}
-      <Calculator />
+      {/* <Calculator /> */}
+      <ToggleBackground />
     </div>
   );
 };
